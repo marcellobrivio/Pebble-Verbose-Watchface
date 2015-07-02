244fb4d5c71968e39384235cfb1881fcbf09a522
marcellobrivio <marcello.brivio@gmail.com>
2015-07-02 20:40:30 +0200

    Pebble Verbose Watchface v0.1
    First Alpha Release.

---
+++ src/main.c
@@ -0,0 +1,83 @@
+/*
+----------------------------
+VERBOSE WATCHFACE FOR PEBBLE
+----------------------------
+
+AUTHOR: Marcello Brivio
+WEBSITE: http://www.marcellobrivio.com
+*/
+
+#include <pebble.h>
+  
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[200];
+
+  // Write the current hours and minutes into the buffer
+  strftime(buffer, sizeof buffer, "STANDARD TIME: %H:%M%nAMERICAN TIME: %I:%M %p%nCOUNT SECONDS: %S/59%nDATE: %F%nWEEKDAY: %A (%u/7)%nWEEK NUMBER: %W/53%nDAY NUMBER: %j/365%nYEAR PERCENTAGE: %C%%", tick_time);
+    
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void main_window_load(Window *window) {
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorClear);
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+  
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
