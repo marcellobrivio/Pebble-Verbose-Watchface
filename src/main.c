0e8819fd4f9df6dd02cefd8925ff45d847559573
marcellobrivio <marcello.brivio@gmail.com>
2015-07-03 13:07:04 +0200

    Pebble Verbose Watchface v0.3
    Added Bluetooth connection check.

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static int s_uptime = 0;
 static TextLayer *s_uptime_layer;
+static TextLayer *s_bluetooth_layer;
 
 // TIME AND DATE //
 static void update_time() {
@@ -30,6 +31,16 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
+// BLUETOOTH CONNECTION CHECKER
+static void bt_handler(bool connected) {
+  // Show current connection state
+  if (connected) {
+    text_layer_set_text(s_bluetooth_layer, "CONNECTED (Y/N): Y");
+  } else {
+    text_layer_set_text(s_bluetooth_layer, "CONNECTED (Y/N): N");
+  }
+}
+
 // WINDOW CREATION //
 static void main_window_load(Window *window) {
   // Create time TextLayer
@@ -44,13 +55,25 @@ static void main_window_load(Window *window) {
   s_uptime_layer = text_layer_create(GRect(0, 98, 144, 20));
   text_layer_set_background_color(s_uptime_layer, GColorBlack);
   text_layer_set_text_color(s_uptime_layer, GColorClear);
-  text_layer_set_text(s_uptime_layer, "UPTIME: 0h 0m 0s");
+  text_layer_set_text(s_uptime_layer, "LOG UPTIME: 0h 0m 0s");
   text_layer_set_font(s_uptime_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
   text_layer_set_text_alignment(s_uptime_layer, GTextAlignmentLeft);
+  
+  // Create Bluetooth TextLayer
+  s_bluetooth_layer = text_layer_create(GRect(0, 112, 144, 20));
+  text_layer_set_background_color(s_bluetooth_layer, GColorBlack);
+  text_layer_set_text_color(s_bluetooth_layer, GColorClear);
+  text_layer_set_text(s_bluetooth_layer, "CONNECTED (Y/N): N");
+  text_layer_set_font(s_bluetooth_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(s_bluetooth_layer, GTextAlignmentLeft);
+  
+  // Show current connection state
+  bt_handler(bluetooth_connection_service_peek());
 
   // Add child layers to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_uptime_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_bluetooth_layer));
   
   // Make sure the time is displayed from the start
   update_time();
@@ -58,9 +81,10 @@ static void main_window_load(Window *window) {
 
 // WINDOW DESTRUCTION //
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
+  // Destroy TextLayers
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_uptime_layer);
+  text_layer_destroy(s_bluetooth_layer);
 }
 
 // TIME-RECURSIVE FUNCTIONS
@@ -78,7 +102,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   int hours = s_uptime / 3600;
 
   // Update the TextLayer
-  snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "UPTIME: %dh %dm %ds", hours, minutes, seconds);
+  snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "LOG UPTIME: %dh %dm %ds", hours, minutes, seconds);
   text_layer_set_text(s_uptime_layer, s_uptime_buffer);
   
   // Increment s_uptime
@@ -97,6 +121,9 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  
+  // Subscribe to Bluetooth updates
+  bluetooth_connection_service_subscribe(bt_handler);
   
   // Register with TickTimerService
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
